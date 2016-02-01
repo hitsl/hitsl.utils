c9fbf5ff46b7bee6bb017f6b579b1933ca58d029
Михаил Мальков <m.malkov@bars-open.ru>
2016-02-01 11:57:27 +0300

    Fix setup.py

--- setup.py
+++ setup.py
@@ -12,9 +12,9 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='hitsl.utils',
+    name='hitsl_utils',
     version='0.1.0',
-    url='https://bitbucket.org/hitsl/hitsl.utils',
+    url='https://github.org/hitsl/hitsl.utils',
     author='hitsl',
     description='Small utilities used in HITSL',
     long_description=read('README.md'),
